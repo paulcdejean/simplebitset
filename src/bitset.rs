5bc6a977f2482fc5bbfdd6b268aa3c36d86004d3
Paul Dejean <paulcdejean@gmail.com>
2025-10-29 20:30:57 -0500

    Improved docs (#16)

--- src/bitset.rs
+++ src/bitset.rs
@@ -1,4 +1,4 @@
-/// A simple BitSet is like a HashMap<u8> except much much faster. The interface is very similar to HashMap.
+/// A simple BitSet is like a `HashMap<u8>` except much much faster. The interface is very similar to HashMap.
 /// Whether or not it implements `Copy` can be toggled with a feature.
 #[derive(PartialEq, Eq, Clone, Hash)]
 #[cfg_attr(feature = "copy", derive(Copy))]
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+//! # Simple BitSet
+//! Basically a `HashSet<u8>` but faster. Even though for a HashSet operations run in O(1) time, for a T that has a limited number of possible
+//! values, there's faster approaches than hashing. A BitSet works by storing each element in a single bit, then using very fast bitwise
+//! operations in order to perform set functions on those bits.
+//!
+//! Simple BitSet is an implementation of BitSet that is purposefully not generic. It only stores u8s. This means less and easier to read
+//! code, and it allows optimization work to be more focused.
+//!
+//! If your use case involves some type that can be represented internally in a single byte, such as a enum with Repr u8, or ASCII chars,
+//! and you wish to use Simple BitSet, then I'd recommend you make a Newtype.
+//!
+//! If you need to store a type that has more than 256 possible values, this crate isn't a good match for that use case.
+//!
+//! ### Features
+//! * `copy` - Enabled by default. Causes BitSet to implement the Copy trait.
+
 mod bitset;
 mod contains;
 mod debug;
@@ -26,3 +42,5 @@ mod take;
 mod union;
 
 pub use bitset::BitSet;
+pub use into_iter::IntoIter;
+pub use iter::Iter;
