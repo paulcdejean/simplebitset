f218af5bfe75a1649323532c116df6691aedad05
Paul Dejean <paulcdejean@gmail.com>
2025-10-29 19:04:14 -0500

    fmt oop

--- src/new.rs
+++ src/new.rs
@@ -7,7 +7,7 @@ impl BitSet {
     }
     /// Creates a new bitset that contains every u8 value.
     pub fn total_set() -> Self {
-        Self([u64::MAX ; 4])
+        Self([u64::MAX; 4])
     }
 }
 
