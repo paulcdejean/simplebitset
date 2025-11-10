7b79330d453e206d3fcfc4712a933f63c89111e5
Paul Dejean <paulcdejean@gmail.com>
2025-11-10 15:42:28 -0600

    50 lines of me losing my mind.

--- benches/insert.rs
+++ benches/insert.rs
@@ -1,73 +1,79 @@
-use core::hint::black_box;
-use criterion::{Criterion, criterion_group, criterion_main};
-
-use simplebitset::BitSet;
-
+use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 use rand::prelude::*;
 
-fn many_inserts() {
-    let mut bs: BitSet = black_box(BitSet::new());
-    for _ in 0..1_00 {
-        for z in 0..255 {
-            black_box(black_box(&mut bs).insert(black_box(z)));
+#[derive(PartialEq, Eq, Clone, Hash, Copy)]
+pub struct BitSet(pub(crate) [u64; 4]);
+
+impl BitSet {
+    pub fn new() -> Self {
+        Self([0; 4])
+    }
+    pub fn insert_blind_branching(&mut self, value: u8) {
+        match value {
+            0..=63 => {
+                let nth: u64 = 1 << (value - 192);
+                self.0[0] |= nth;
+            }
+            64..=127 => {
+                let nth: u64 = 1 << (value - 192);
+                self.0[1] |= nth;
+            }
+            128..=191 => {
+                let nth: u64 = 1 << (value - 192);
+                self.0[2] |= nth;
+            }
+            192..=255 => {
+                let nth: u64 = 1 << (value - 192);
+                self.0[3] |= nth;
+            }
         }
     }
+    pub fn insert_blind_branchless(&mut self, value: u8) {
+        let index: u8 = value / 64;
+        let offset: u8 = value % 64;
+        // SAFETY: a u8 divided by 64 is between 0 and 3. MIR doesn't know this though.
+        let num: &mut u64 = unsafe { self.0.get_unchecked_mut(index as usize) };
+        let mask: u64 = 1 << offset;
+        *num |= mask;
+    }
 }
 
-fn insert_vec(input: &Vec<u8>) {
-    let mut bs: BitSet = black_box(BitSet::new());
+fn insert_vec_branching(input: &Vec<u8>) {
+    let mut bs: BitSet = BitSet::new();
     for element in input {
-        black_box(black_box(&mut bs).insert(black_box(*element)));
+        bs.insert_blind_branching(*element);
     }
 }
 
-fn double_insert() {
-    let mut bs: BitSet = black_box(BitSet::new());
-    for n in 0..=255 {
-        black_box(black_box(&mut bs).insert(black_box(n)));
-        black_box(black_box(&mut bs).insert(black_box(n)));
-    }
-}
-
-fn many_inserts_v2() {
-    let mut bs: BitSet = black_box(BitSet::new());
-    for _ in 0..1_00 {
-        for z in 0..255 {
-            black_box(black_box(&mut bs).insert_v2(black_box(z)));
-        }
-    }
-}
-
-fn insert_vec_v2(input: &Vec<u8>) {
-    let mut bs: BitSet = black_box(BitSet::new());
+fn insert_vec_branchless(input: &Vec<u8>) {
+    let mut bs: BitSet = BitSet::new();
     for element in input {
-        black_box(black_box(&mut bs).insert_v2(black_box(*element)));
-    }
-}
-
-fn double_insert_v2() {
-    let mut bs: BitSet = black_box(BitSet::new());
-    for n in 0..=255 {
-        black_box(black_box(&mut bs).insert_v2(black_box(n)));
-        black_box(black_box(&mut bs).insert_v2(black_box(n)));
+        bs.insert_blind_branchless(*element);
     }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut rng = rand::rng();
-
     let mut input: Vec<u8> = Vec::new();
 
     for _ in 0..200 {
         input.push(rng.random::<u8>());
     }
 
-    c.bench_function("insert rand vec v1", |b| b.iter(|| insert_vec(black_box(&input))));
-    c.bench_function("many inserts v1", |b| b.iter(|| many_inserts()));
-    c.bench_function("double insert v1", |b| b.iter(|| double_insert()));
-    c.bench_function("insert rand vec v2", |b| b.iter(|| insert_vec_v2(black_box(&input))));
-    c.bench_function("many inserts v2", |b| b.iter(|| many_inserts_v2()));
-    c.bench_function("double insert v2", |b| b.iter(|| double_insert_v2()));
+    c.bench_with_input(
+        BenchmarkId::new("insert rand vec branching", "200 random numbers"),
+        &input,
+        |b, i| {
+            b.iter(|| insert_vec_branching(&i));
+        },
+    );
+    c.bench_with_input(
+        BenchmarkId::new("insert rand vec branchless", "200 random numbers"),
+        &input,
+        |b, i| {
+            b.iter(|| insert_vec_branchless(&i));
+        },
+    );
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/insert.rs
+++ src/insert.rs
@@ -54,10 +54,46 @@ impl BitSet {
         let index: u8 = value / 64;
         let offset: u8 = value % 64;
         // SAFETY: a u8 divided by 64 is between 0 and 3. MIR doesn't know this though.
+        let num: u64 = *unsafe { self.0.get_unchecked(index as usize) };
+        let mask: u64 = 1 << offset;
+        let result: u64 = num | mask;
+        if result - num == 0 {
+            false
+        } else {
+            *unsafe { self.0.get_unchecked_mut(index as usize) } = result;
+            true
+        }
+    }
+
+    #[inline(always)]
+    pub fn insert_blind_nobranch(&mut self, value: u8) {
+        let index: u8 = value / 64;
+        let offset: u8 = value % 64;
+        // SAFETY: a u8 divided by 64 is between 0 and 3. MIR doesn't know this though.
         let num: &mut u64 = unsafe { self.0.get_unchecked_mut(index as usize) };
         let mask: u64 = 1 << offset;
-        let is_inserted: bool = (*num & mask) == 0;
         *num |= mask;
-        is_inserted
+    }
+
+    #[inline(always)]
+    pub fn insert_blind_branching(&mut self, value: u8) {
+        match value {
+            0..=63 => {
+                let nth: u64 = 1 << (value - 192);
+                self.0[0] |= nth;
+            }
+            64..=127 => {
+                let nth: u64 = 1 << (value - 192);
+                self.0[1] |= nth;
+            }
+            128..=191 => {
+                let nth: u64 = 1 << (value - 192);
+                self.0[2] |= nth;
+            }
+            192..=255 => {
+                let nth: u64 = 1 << (value - 192);
+                self.0[3] |= nth;
+            }
+        }
     }
 }
