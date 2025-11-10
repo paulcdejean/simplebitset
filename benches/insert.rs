6541b5ce9ea059eddfb564adebaf1b135e44faad
Paul Dejean <paulcdejean@gmail.com>
2025-11-10 15:59:37 -0600

    Ok done.

--- benches/insert.rs
+++ benches/insert.rs
@@ -1,47 +1,11 @@
 use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 use rand::prelude::*;
-
-#[derive(PartialEq, Eq, Clone, Hash, Copy)]
-pub struct BitSet(pub(crate) [u64; 4]);
-
-impl BitSet {
-    pub fn new() -> Self {
-        Self([0; 4])
-    }
-    pub fn insert_blind_branching(&mut self, value: u8) {
-        match value {
-            0..=63 => {
-                let nth: u64 = 1 << value;
-                self.0[0] |= nth;
-            }
-            64..=127 => {
-                let nth: u64 = 1 << (value - 64);
-                self.0[1] |= nth;
-            }
-            128..=191 => {
-                let nth: u64 = 1 << (value - 128);
-                self.0[2] |= nth;
-            }
-            192..=255 => {
-                let nth: u64 = 1 << (value - 192);
-                self.0[3] |= nth;
-            }
-        }
-    }
-    pub fn insert_blind_branchless(&mut self, value: u8) {
-        let index: u8 = value / 64;
-        let offset: u8 = value % 64;
-        // SAFETY: a u8 divided by 64 is between 0 and 3. MIR doesn't know this though.
-        let num: &mut u64 = unsafe { self.0.get_unchecked_mut(index as usize) };
-        let mask: u64 = 1 << offset;
-        *num |= mask;
-    }
-}
+use simplebitset::BitSet;
 
 fn insert_vec_branching(input: &Vec<u8>) -> BitSet {
     let mut bs: BitSet = BitSet::new();
     for element in input {
-        bs.insert_blind_branching(*element);
+        bs.insert_old(*element);
     }
     bs
 }
@@ -49,7 +13,7 @@ fn insert_vec_branching(input: &Vec<u8>) -> BitSet {
 fn insert_vec_branchless(input: &Vec<u8>) -> BitSet {
     let mut bs: BitSet = BitSet::new();
     for element in input {
-        bs.insert_blind_branchless(*element);
+        bs.insert(*element);
     }
     bs
 }
--- src/insert.rs
+++ src/insert.rs
@@ -8,6 +8,21 @@ impl BitSet {
     /// - If the set did not previously contain this value, `true` is returned.
     /// - If the set already contained this value, `false` is returned.
     pub fn insert(&mut self, value: u8) -> bool {
+        let index: u8 = value / 64;
+        let offset: u8 = value % 64;
+        // SAFETY: a u8 divided by 64 is between 0 and 3. MIR doesn't know this though.
+        let num: &mut u64 = unsafe { self.0.get_unchecked_mut(index as usize) };
+        let mask: u64 = 1 << offset;
+        if *num & mask == 0 {
+            *num |= mask;
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Older version of insert with worse performance.
+    pub fn insert_old(&mut self, value: u8) -> bool {
         match value {
             0..=63 => {
                 let nth: u64 = 1 << value;
@@ -47,53 +62,25 @@ impl BitSet {
             }
         }
     }
+}
 
-    /// This version of insert is much fewer instructions, but seems to struggle in benchmarks.
-    /// You're free to use it, especially if it performs faster in your own benchmarks.
-    pub fn insert_v2(&mut self, value: u8) -> bool {
-        let index: u8 = value / 64;
-        let offset: u8 = value % 64;
-        // SAFETY: a u8 divided by 64 is between 0 and 3. MIR doesn't know this though.
-        let num: u64 = *unsafe { self.0.get_unchecked(index as usize) };
-        let mask: u64 = 1 << offset;
-        let result: u64 = num | mask;
-        if result - num == 0 {
-            false
-        } else {
-            *unsafe { self.0.get_unchecked_mut(index as usize) } = result;
-            true
-        }
-    }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    #[inline(always)]
-    pub fn insert_blind_nobranch(&mut self, value: u8) {
-        let index: u8 = value / 64;
-        let offset: u8 = value % 64;
-        // SAFETY: a u8 divided by 64 is between 0 and 3. MIR doesn't know this though.
-        let num: &mut u64 = unsafe { self.0.get_unchecked_mut(index as usize) };
-        let mask: u64 = 1 << offset;
-        *num |= mask;
-    }
-
-    #[inline(always)]
-    pub fn insert_blind_branching(&mut self, value: u8) {
-        match value {
-            0..=63 => {
-                let nth: u64 = 1 << (value - 192);
-                self.0[0] |= nth;
-            }
-            64..=127 => {
-                let nth: u64 = 1 << (value - 192);
-                self.0[1] |= nth;
-            }
-            128..=191 => {
-                let nth: u64 = 1 << (value - 192);
-                self.0[2] |= nth;
-            }
-            192..=255 => {
-                let nth: u64 = 1 << (value - 192);
-                self.0[3] |= nth;
-            }
-        }
+    #[test]
+    fn test_insert() {
+        let mut example: BitSet = BitSet::new();
+        assert!(example.insert(0));
+        assert!(!example.insert(0));
+        assert!(example.contains(0));
+        assert!(!example.contains(1));
+        assert!(!example.contains(255));
+        assert!(example.insert(69));
+        assert!(!example.insert(69));
+        assert!(example.contains(69));
+        assert!(example.insert(255));
+        assert!(!example.insert(255));
+        assert!(example.contains(255));
     }
 }
