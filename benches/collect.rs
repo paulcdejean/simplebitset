c8828c70a16a8f350a0857f25caf7820aea59ef6
Paul Dejean <paulcdejean@gmail.com>
2025-11-13 20:07:18 -0500

    Added some comparison notes.

--- benches/collect.rs
+++ benches/collect.rs
@@ -1,13 +1,12 @@
 use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 use rand::prelude::*;
-use simplebitset::BitSet;
 
-fn collect_bitset(input: &Vec<u8>) -> Vec<u8> {
-    let mut bs: BitSet = BitSet::new();
+fn collect_simplebitset(input: &Vec<u8>) -> Vec<u8> {
+    let mut bs: simplebitset::BitSet = simplebitset::BitSet::new();
     for element in input {
         bs.insert(*element);
     }
-    bs.iter().collect()
+    bs.into_iter().collect()
 }
 
 fn collect_hashset(input: &Vec<u8>) -> Vec<u8> {
@@ -15,25 +14,38 @@ fn collect_hashset(input: &Vec<u8>) -> Vec<u8> {
     for element in input {
         bs.insert(*element);
     }
-    bs.iter().map(|x| *x).collect()
+    bs.into_iter().collect()
+}
+
+fn collect_bit_set(input: &Vec<u8>) -> Vec<u8> {
+    let mut bs: bit_set::BitSet<u32> = bit_set::BitSet::with_capacity(256);
+    for element in input {
+        bs.insert(*element as usize);
+    }
+    bs.into_iter().map(|x| x as u8).collect()
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut rng = rand::rng();
     let mut input: Vec<u8> = Vec::new();
 
-    for _ in 0..100 {
+    for _ in 0..300 {
         input.push(rng.random::<u8>());
     }
     c.bench_with_input(
-        BenchmarkId::new("collect bits bitset", "100 random numbers"),
+        BenchmarkId::new("collect bits simplebitset", "300 random numbers"),
         &input,
-        |b, i| b.iter(|| collect_bitset(&i)),
+        |b, i| b.iter(|| collect_simplebitset(&i)),
     );
     c.bench_with_input(
-        BenchmarkId::new("collect bits hashset", "100 random numbers"),
+        BenchmarkId::new("collect bits hashset", "300 random numbers"),
         &input,
         |b, i| b.iter(|| collect_hashset(&i)),
+    );
+    c.bench_with_input(
+        BenchmarkId::new("collect bits bit_set", "300 random numbers"),
+        &input,
+        |b, i| b.iter(|| collect_bit_set(&i)),
     );
 }
 
